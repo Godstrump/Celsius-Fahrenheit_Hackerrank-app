d8f571faeab0b915da57bafc6ae466174b47c7db
Godstrump <GGodstrump@gmail.com>
2021-10-11 10:00:46 +0100

    Update

--- src/app/temperatureConverter/temperatureConverter.component.ts
+++ src/app/temperatureConverter/temperatureConverter.component.ts
@@ -1,5 +1,4 @@
 import { Component, ElementRef, Input, OnInit, ViewChild } from "@angular/core";
-import { AbstractControl, FormControl, FormGroup } from "@angular/forms";
 
 @Component({
   selector: "temperature-converter",
@@ -7,33 +6,39 @@ import { AbstractControl, FormControl, FormGroup } from "@angular/forms";
   styleUrls: ["./temperatureConverter.component.scss"],
 })
 export class TemperatureConverter implements OnInit {
-  @ViewChild("cel") cels: ElementRef;
-  @ViewChild("fah") fahr: ElementRef;
+  c: number;
+  f: number;
 
   ngOnInit() {
     // C = (F − 32) × 5/9
     // F = C*9/5 + 32
   }
 
-  convertCelsius(value) {
-    if (value)
-      return (this.fahr.nativeElement.value =
-        Math.round(((value * 9) / 5 + 32) * 10) / 10);
-    else {
-      value = 0;
-      return (this.fahr.nativeElement.value =
-        Math.round(((value * 9) / 5 + 32) * 10) / 10);
+  convertCelsius(value: number) {
+    if (!value) {
+      this.f = undefined;
+      return;
+    }
+    let fahrenheit = (value * 9) / 5 + 32;
+
+    if (fahrenheit % 2 != 0) {
+      this.f = parseFloat(fahrenheit.toFixed(1));
+    } else {
+      this.f = fahrenheit;
     }
   }
 
-  convertFahrenheit(value) {
-    if (value)
-      return (this.cels.nativeElement.value =
-        Math.round((((value - 32) * 5) / 9) * 10) / 10);
-    else {
-      value = 0;
-      return (this.cels.nativeElement.value =
-        Math.round((((value - 32) * 5) / 9) * 10) / 10);
+  convertFahrenheit(value: number) {
+    if (!value) {
+      this.c = undefined;
+      return;
+    }
+    let celsius = ((value - 32) * 5) / 9;
+
+    if (celsius % 2 != 0) {
+      this.c = parseFloat(celsius.toFixed(1));
+    } else {
+      this.c = celsius;
     }
   }
 }
